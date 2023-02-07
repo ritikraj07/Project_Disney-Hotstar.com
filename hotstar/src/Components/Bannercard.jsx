ee996482162a36169a577a5b7fcb2187727288ab
sayyadshoaib <shoaibsayyad145@gmail.com>
2023-02-08 00:24:58 +0530

    Banner Added

--- hotstar/src/Components/Bannercard.jsx
+++ hotstar/src/Components/Bannercard.jsx
@@ -8,7 +8,6 @@ function Bannercard() {
           <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, sint autem sapiente quasi natus atque ratione quam, totam veritatis animi est odit minima. Distinctio perferendis repellendus iste sapiente nostrum quod?</h3>
             </div>
             <div className={Style.child2}>
-                {/* <img src="https://images7.alphacoders.com/128/1280283.jpg" alt="" className={ Style.Container} /> */}
             </div>    
       </div>
 
