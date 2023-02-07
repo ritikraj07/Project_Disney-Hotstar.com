1eed9a7b95bfe1582e8ccfc7d4695154d7afa8d6
ritikraj07 <ritikra3rrr@gmail.com>
2023-02-07 13:20:44 +0530

    file add

---
+++ hotstar/src/Pages/landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '../Components/Card';
+
+function Landing(props) {
+    return (
+        <div>
+            {<Card />}
+        </div>
+    );
+}
+
+export default Landing;
