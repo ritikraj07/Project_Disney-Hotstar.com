de4c7473ce971987e48e34fb7ad3fdb2062b2b74
sayyadshoaib <shoaibsayyad145@gmail.com>
2023-02-07 13:36:41 +0530

    BannerComponentAdded

---
+++ hotstar/src/Components/Bannercard.jsx
@@ -0,0 +1,17 @@
+function Bannercard() {
+    return (
+        <section>
+            <div>
+                <h2>Black Panthar : Wakanda Forever</h2>
+                <p>Action : 2022</p>
+                <h4>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus quae vero consectetur corrupti, suscipit quos eos nam minus expedita, quod inventore omnis voluptate voluptatem veniam sunt aliquid dicta fuga unde.
+                </h4>
+            </div>
+            <div>
+                <img src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/blackpantherwakandaforever_lob_mas_mob_04.jpg" alt="" />
+            </div>
+       </section>
+   ) 
+}
+export default Bannercard;
