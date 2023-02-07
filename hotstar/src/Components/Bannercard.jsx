43a1113d94c8cac24c3726270b59bf7be0d788c6
sayyadshoaib <shoaibsayyad145@gmail.com>
2023-02-07 20:12:50 +0530

    banner

--- hotstar/src/Components/Bannercard.jsx
+++ hotstar/src/Components/Bannercard.jsx
@@ -1,17 +1,24 @@
 function Bannercard() {
-    return (
-        <section>
-            <div>
-                <h2>Black Panthar : Wakanda Forever</h2>
-                <p>Action : 2022</p>
-                <h4>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus quae vero consectetur corrupti, suscipit quos eos nam minus expedita, quod inventore omnis voluptate voluptatem veniam sunt aliquid dicta fuga unde.
-                </h4>
-            </div>
-            <div>
-                <img src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/blackpantherwakandaforever_lob_mas_mob_04.jpg" alt="" />
-            </div>
-       </section>
-   ) 
+      return (
+    <section style={BannerSection}>
+      <div style={{ width: "40%", margin: "auto", padding: "20px" }}>
+        <h2>Black Panthar : Wakanda Forever</h2>
+        <p>Action : 2022</p>
+        <h4>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus
+          quae vero consectetur corrupti, suscipit quos eos nam minus expedita,
+          quod inventore omnis voluptate voluptatem veniam sunt aliquid dicta
+          fuga unde.
+        </h4>
+      </div>
+      <div style={{ width: "60%", margin: "auto" }}>
+        <img
+          src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/blackpantherwakandaforever_lob_mas_mob_04.jpg"
+          alt=""
+          style={{ width: "100%" }}
+        />
+      </div>
+    </section>
+  );
 }
 export default Bannercard;
