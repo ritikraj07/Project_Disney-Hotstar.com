b1294403525f319d95c7c31a1679ce0bda733bcd
Vaishnavi Ralegaonkar <ralegaonkarvaishnavi@gmail.com>
2023-02-08 22:37:23 +0530

    subscribe b page

--- hotstar/src/Pages/Subscribe.jsx
+++ hotstar/src/Pages/Subscribe.jsx
@@ -1,11 +1,12 @@
 import "../CSS/subscribe.css";
-import Footer from "../Components/footer";
+// import Footer from "../Components/footer";
 
 const Subscribe = () => {
 
     return (
         <div style={{
             backgroundImage: "linear-gradient(rgb(12,17,27), rgb(19,19,25))",
+           
 
         }}>
             <div className="Navigation-bar" >
@@ -17,40 +18,39 @@ const Subscribe = () => {
                     <select name="language" id="lang">
                         <option value="ENGLISH">ENGLISH</option>
                         <option value="HINDI">हिंदी</option>
-
                     </select>
                     <button  className="loginbutton">Log In</button>
                 
             </div>
 
-            <h2>Subscribe to watch all content on Disney+ Hotstar </h2>
+            <h2 className="heading">Subscribe to watch all content on Disney+ Hotstar </h2>
             <div className="sub_div">
                 <table style={{width:"100%",}}>
                     <tr height ="30px">
-                        <th style={{width:"70%", }}></th>
+                        <th style={{width:"70%", textAlign:"center"}} ></th>
                         <th>Super</th>
                         <th>Premium</th>
                     </tr>
                     <tr height ="30px">
                         <td>All content <h6>Movies, live sports, TV, Specials</h6></td>
-                        <td><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/tick/smp/web-1x.png" alt="tick" /></td>
-                        <td><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/tick/smp/web-1x.png" alt="tick" /></td>
+                        <td align="center"><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/tick/smp/web-1x.png" alt="tick" /></td>
+                        <td align="center"><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/tick/smp/web-1x.png" alt="tick" /></td>
                     </tr>
 
                     <tr height ="30px">
                         <td>Watch on TV or Laptop</td>
-                        <td><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/tick/smp/web-1x.png" alt="tick" /></td>
-                        <td><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/tick/smp/web-1x.png" alt="tick" /></td>
+                        <td align="center"><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/tick/smp/web-1x.png" alt="tick" /></td>
+                        <td align="center"><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/tick/smp/web-1x.png" alt="tick" /></td>
                     </tr>
                     <tr height ="30px">
                         <td>Ads free movies and shows (except sports)</td>
-                        <td><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/cross/smp/web-1x.png" alt="cross" /></td>
-                        <td><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/tick/smp/web-1x.png" alt="tick" /></td>
+                        <td align="center"><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/cross/smp/web-1x.png" alt="cross" /></td>
+                        <td align="center"><img src="https://secure-media.hotstar.com/static/subscription/paywall_service/comparator/tick/smp/web-1x.png" alt="tick" /></td>
                     </tr>
                     <tr height ="30px">
                         <td>Number of devices that can be logged in</td>
-                        <td>2</td>
-                        <td>4</td>
+                        <td align="center">2</td>
+                        <td align="center">4</td>
                     </tr>
                     <tr height ="30px">
                         <td>Max video quality</td>
@@ -67,7 +67,7 @@ const Subscribe = () => {
 
                 </table>
                 <div className="planbutton">
-                    <button>
+                    <button >
                         <h3>Super</h3>
                         <h2>₹899/Year</h2>
                     </button>
@@ -84,7 +84,7 @@ const Subscribe = () => {
             </div>
 
             <div className="sub_footer">
-                <Footer/>
+                {/* <Footer/> */}
             </div>
         </div>
         
