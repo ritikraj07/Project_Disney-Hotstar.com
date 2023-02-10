84f0a71301dfb67237bfffcffb736280a428082b
Anshika Mishra <33021256+anshika1297@users.noreply.github.com>
2023-02-11 04:07:56 +0530

    Add files via upload

---
+++ hotstar/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Landing from './Pages/landing';
+import Subscribe from './Pages/Subscribe';
+
+
+
+function App() {
+  return (
+    <div>
+      {/* <Landing /> */}
+      <Subscribe />
+    </div>
+
+  );
+}
+
+export default App;
